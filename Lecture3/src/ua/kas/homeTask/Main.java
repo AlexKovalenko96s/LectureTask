7d5d9bcd7c7b4e037a5321780ffc6c11a5c14f75
Alex Kovalenko <alex.kovalenko.s96@gmail.com>
2016-11-14 22:36:01 +0200

    //

--- Lecture3/src/ua/kas/homeTask/Main.java
+++ Lecture3/src/ua/kas/homeTask/Main.java
@@ -178,23 +178,29 @@ public class Main {
 	public static void eleventh() {
 		System.out.println("-----11-----");
 
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
-				System.out.print("$");
-			}
-			System.out.println();
-		}
-
-		System.out.println("-----");
+		// for (int i = 0; i < 10; i++) {
+		// for (int j = 0; j < 10; j++) {
+		// System.out.print("$");
+		// }
+		// System.out.println();
+		// }
+		//
+		// System.out.println("-----");
+		//
+		// for (int i = 0; i < 10; i++) {
+		// for (int j = i; j < 10; j++) {
+		// System.out.print("$");
+		// }
+		// System.out.println();
+		// }
+		//
+		// System.out.println("-----");
 
 		for (int i = 0; i < 10; i++) {
 			for (int j = i; j < 10; j++) {
-				System.out.print("$");
+
 			}
 			System.out.println();
 		}
-
-		System.out.println("-----");
-
 	}
 }
